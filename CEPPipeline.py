3ff866350bd6dc3fbd71235e5758aa4f4828a90d
Kevin Brown <kevin.s.brown@uconn.edu>
2014-03-11 11:41:08 -0400

    Licensing info added to .py files.

--- CEPPipeline.py
+++ CEPPipeline.py
@@ -1,15 +1,46 @@
 """
 CEPPipeline.py
 
-Created by CA Brown and KS Brown.  For reference see:
-Brown, C.A., Brown, K.S. 2010. Validation of coevolving residue algorithms via 
-pipeline sensitivity analysis: ELSC and OMES and ZNMI, oh my! PLoS One, e10779. 
-doi:10.1371/journal.pone.0010779
-
 This module is used to control all of the pipeline flow and do 
 the reshuffling, etc.  It works with the CEPAlgorithms module which can 
 easily be adapted to include more algorithms.  The main pipeline is initialized 
 with all of the information for the rest of the project. 
+
+This is a pure python package for correlated substitution analysis.  Specifically, it can be 
+used for the kinds of analyses in the following publication:
+
+"Validation of coevolving residue algorithms via pipeline sensitivity analysis: ELSC and 
+OMES and ZNMI, oh my!" PLoS One, e10779. doi:10.1371/journal.pone.0010779 (2010)
+
+@author: Kevin S. Brown (University of Connecticut), Christopher A. Brown (Palomidez LLC)
+
+This source code is provided under the BSD-3 license, duplicated as follows:
+
+Copyright (c) 2014, Kevin S. Brown and Christopher A. Brown
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this 
+list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this 
+list of conditions and the following disclaimer in the documentation and/or other 
+materials provided with the distribution.
+
+3. Neither the name of the University of Connecticut nor the names of its contributors 
+may be used to endorse or promote products derived from this software without specific 
+prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS 
+OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY 
+AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER 
+IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT 
+OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
 import sys, os, unittest, random, glob, cPickle, re, itertools
