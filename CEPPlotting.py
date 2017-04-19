ca8ed2c8e25b37f76d70905d365957fff551d830
Kevin Brown <kevin.s.brown@uconn.edu>
2017-04-19 09:51:06 -0400

    Added network plotting functions.  Bump to 1.1.0

--- CEPPlotting.py
+++ CEPPlotting.py
@@ -1,14 +1,14 @@
 """
 CEPPlotting.py
 
-This module is used to make all of the plots for 
+This module is used to make all of the plots for
 CA Brown, KS Brown, (2010) PLoS One.  They are all more or
-less built up from matplotlib and pylab. 
+less built up from matplotlib and pylab.
 
-This is a pure python package for correlated substitution analysis.  Specifically, it can be 
+This is a pure python package for correlated substitution analysis.  Specifically, it can be
 used for the kinds of analyses in the following publication:
 
-"Validation of coevolving residue algorithms via pipeline sensitivity analysis: ELSC and 
+"Validation of coevolving residue algorithms via pipeline sensitivity analysis: ELSC and
 OMES and ZNMI, oh my!" PLoS One, e10779. doi:10.1371/journal.pone.0010779 (2010)
 
 @author: Kevin S. Brown (University of Connecticut), Christopher A. Brown (Palomidez LLC)
@@ -18,33 +18,34 @@ This source code is provided under the BSD-3 license, duplicated as follows:
 Copyright (c) 2014, Kevin S. Brown and Christopher A. Brown
 All rights reserved.
 
-Redistribution and use in source and binary forms, with or without modification, 
+Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
 
-1. Redistributions of source code must retain the above copyright notice, this 
+1. Redistributions of source code must retain the above copyright notice, this
 list of conditions and the following disclaimer.
 
-2. Redistributions in binary form must reproduce the above copyright notice, this 
-list of conditions and the following disclaimer in the documentation and/or other 
+2. Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or other
 materials provided with the distribution.
 
-3. Neither the name of the University of Connecticut nor the names of its contributors 
-may be used to endorse or promote products derived from this software without specific 
+3. Neither the name of the University of Connecticut nor the names of its contributors
+may be used to endorse or promote products derived from this software without specific
 prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS 
-OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY 
-AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
-CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER 
-IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT 
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
 OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
 import sys, os, unittest, pylab, math, numpy, copy, networkx, scipy
 from scipy.stats import gaussian_kde
 from matplotlib.ticker import FuncFormatter
+import networkx as nx
 
 
 basic_colors = ('b', 'g', 'r', 'c', 'm', 'k','y')
@@ -57,7 +58,7 @@ def ColorWheel(colors = basic_colors, symbols = basic_symbols,
     """
     ColorWheel()
 
-    Returns a generator that cycles through a selection of colors, symbols, and 
+    Returns a generator that cycles through a selection of colors, symbols, and
     line styles for matlibplot.matlab.plot.
     """
     if not colors:
@@ -75,7 +76,7 @@ def ColorWheel(colors = basic_colors, symbols = basic_symbols,
 
 
 class CEPPlotting(object):
-    """This is the main plotting class for making scatter histograms, etc."""
+    """This is the main plotting class for making scatter histograms, network plots, etc."""
     def __init__(self,figDirectory=None,figFormat='pdf'):
         """Directory to save figures.  Defaults to None, which just does a show()."""
         self.figFormat = figFormat
@@ -89,7 +90,7 @@ class CEPPlotting(object):
         self.accMax = None
         self.repMin = None
         self.repMax = None
-    
+
     def acc_formatter(self,x,pos):
         return '%0.2f'%x
 
@@ -103,7 +104,7 @@ class CEPPlotting(object):
         self.accMax = accLimits[1]
         self.repMin = repLimits[0]
         self.repMax = repLimits[1]
-    
+
     def plot_accuracy_reproducibility(self,*args):
         """Makes an accuracy/reproducibilty plot for a list of CEPPipeline objects.  All objects
         are assumed to have the same resampling scheme and definitions for reproducibility and
@@ -124,7 +125,7 @@ class CEPPlotting(object):
             self.cepColors[args[i]] = c
             self.cepPoints[args[i]] = s
             self.cepLines[args[i]] = l
-        # plot and axis locations 
+        # plot and axis locations
         left, width = 0.1, 0.65
         bottom, height = 0.1, 0.65
         bottom_h = left_h = left+width+0.05
@@ -161,9 +162,9 @@ class CEPPlotting(object):
         else:
             pylab.savefig(os.path.join(self.figDirectory,'accuracy_reproducibility')+'.'+self.figFormat,format=self.figFormat)
 
-    
+
     def compute_axis_limits(self, *args):
-        """Determines the scales for accuracy and reproducibility; these vary based on the 
+        """Determines the scales for accuracy and reproducibility; these vary based on the
         particular definition used."""
         self.repMin,self.repMax = 0.0,1.0
         self.accMin,self.accMax = numpy.inf,-numpy.inf
@@ -177,7 +178,7 @@ class CEPPlotting(object):
         self.accMin = self.accMin - self.inflation*abs(self.accMin)
         self.accMax = self.accMax + self.inflation*abs(self.accMax)
 
-    
+
     def plot_acc_rep_scatter(self,scatterAx,*args):
         """Makes a scatterplot of accuracy vs. reproducibility.  Reproducibility is defined for each
         subsample."""
@@ -258,16 +259,113 @@ class CEPPlotting(object):
         histAx.set_xticks([])
 
 
-    # FROM HERE ON OUT THESE MAY NOT WORK - NEED TO BE UPDATED        
+    def ring_plot(self,cep,rcut,ncData=None,layout='twopi'):
+        """
+        Draws a pipeline reproducibility graph and saves it or displays it.
+        The nodes can be positioned on a ring or a series of concentric
+        circles.  In this representation, labels are suppressed.
+
+        Notice that unlike the histogram plotting functions, this function
+        can only plot one pipeline (one consensus network).
+
+        INPUT
+        ------
+        cep     : CEPPipeline object, required
+
+        rcut    : float in [0,1], required
+            Reproducibility threshold for suppressing edges
+
+        ncData  : dict, optional
+            custom color information to decorate nodes; should be a dictionary
+            keyed on nodes, with
+
+        layout  : string, optional
+            suggested layouts are 'circo','twopi',and 'radial'
+        """
+        # figure out which edges to plot
+        edges_to_show = []
+        for e in cep['graph'].edges():
+            if cep['graph'].get_edge_data(e[0],e[1])['weight'] > rcut:
+                edges_to_show.append(e)
+        # construct node colors
+        nList = cep['graph'].nodes()
+        nc = ['k' for x in nList]
+        if ncData is not None:
+            for i in xrange(0,len(nList)):
+                if ncData.has_key(nList[i]):
+                    nc[i] = ncData[nList[i]]
+        # now make the plot
+        pylab.figure(figsize=(8,8))
+        if layout == 'radial':
+            pos = nx.circular_layout(cep['graph'])
+        else:
+            pos = nx.nx_pydot.graphviz_layout(cep['graph'],prog=layout)
+        nx.draw_networkx(cep['graph'],pos=pos,edgelist=edges_to_show,node_color=nc,node_size=25,with_labels=False)
+        pylab.axis('off')
+        if self.figDirectory is None:
+            pylab.show()
+        else:
+            pylab.savefig(os.path.join(self.figDirectory,'network_ring_'+str(rcut).replace('.','p'))+'.'+self.figFormat,format=self.figFormat)
+
+
+    def net_plot(cep,rcut,saveDir='.',ncData=None,layout='neato',cmap=pylab.cm.RdYlGn):
+        """
+        Draws a pipeline reproducibility graph and saves it or displays it.
+        This version uses energy- or force-based layouts, includes node
+        labels, and allows optional color coding as in ring_plot.
+
+        Only supported color coding is floats.
+
+        INPUT
+        ------
+        cep     : CEPPipeline object, required
+
+        rcut    : float in [0,1], required
+            Reproducibility threshold for pruning the pipeline graph
+
+        ncData  : dict, optional
+            custom color information to decorate nodes (needs to be floats!).  The
+            default nodecolor is 0.0, the color of which will depend on cmap.
+
+        layout  : string, optional
+            can be any graphviz layout, but 'neato' or 'fdp' tend to work well
+
+        cmap    : matplotlib.cmap, optional
+            colormap for node display; 'RdYlGn' is the default in the hopes that
+            node labels are still readable anywhere.
+        """
+        # copy the graph and prune it
+        G = copy.deepcopy(cep['graph'])
+        G.prune_graph(rcut)
+        nList = G.nodes()
+        nc = [0.0 for x in nList]
+        if ncData is not None:
+            for i in xrange(0,len(nList)):
+                if ncData.has_key(nList[i]):
+                    nc[i] = ncData[nList[i]]
+        # now make the plot
+        pylab.figure(figsize=(8,8))
+        pos = nx.nx_pydot.graphviz_layout(G,prog=layout)
+        nx.draw_networkx(G,pos=pos,node_color=nc,cmap=cmap,node_size=200,with_labels=True,
+            font_size=6,width=2.0,alpha=0.5)
+        pylab.axis('off')
+        if self.figDirectory is None:
+            pylab.show()
+        else:
+            pylab.savefig(os.path.join(self.figDirectory,'network_spring_'+str(rcut).replace('.','p'))+'.'+self.figFormat,format=self.figFormat)
+
+
+    '''
+    # FROM HERE ON OUT THESE MAY NOT WORK - NEED TO BE UPDATED
     def plot_accuracy_versus_cutoff(self, *args):
         """Makes a plot of accuracy versus cutoff for edge resampling
         Note: it take a variable number of arguments (i.e. pass a list of CEPPipeline objects)"""
-        # setup colors 
+        # setup colors
         colors = 'rgbymck'
         point = ','
         cepColors = {}.fromkeys(args)
         for i in xrange(len(args)):
-            cepColors[args[i]] = colors[int(math.fmod(i,len(colors)))] 
+            cepColors[args[i]] = colors[int(math.fmod(i,len(colors)))]
         cutoffs = numpy.arange(0,1,0.005).tolist()
         for cep in args:
             distances = list()
@@ -286,16 +384,16 @@ class CEPPlotting(object):
         else:
             pylab.savefig(os.path.join(self.figDirectory,'accuracy_cutoff')+'.'+self.figFormat,format=self.figFormat)
 
-            
+
     def plot_accuracy_versus_percolation(self, *args):
         """Makes a plot of accuracy versus percolation (largest connected component) for edge resampling
         Note: it take a variable number of arguments (i.e. pass a list of CEPPipeline objects)"""
-        # setup colors 
+        # setup colors
         colors = 'rgbymck'
         point = ','
         cepColors = {}.fromkeys(args)
         for i in xrange(len(args)):
-            cepColors[args[i]] = colors[int(math.fmod(i,len(colors)))] 
+            cepColors[args[i]] = colors[int(math.fmod(i,len(colors)))]
         cutoffs = numpy.arange(0,1,0.005).tolist()
         for cep in args:
             percolation = list()
@@ -312,7 +410,7 @@ class CEPPlotting(object):
             pylab.show()
         else:
             pylab.savefig(os.path.join(self.figDirectory,'accuracy_percolation')+'.'+self.figFormat,format=self.figFormat)
-
+    '''
 
 class CEPPlottingTests(unittest.TestCase):
     def setUp(self):
